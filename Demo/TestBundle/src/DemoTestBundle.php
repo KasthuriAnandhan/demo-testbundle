7780e3e53676a67629a0ddb6816a071198d21a6a
KasthuriAnandhan <125338676+KasthuriAnandhan@users.noreply.github.com>
2024-08-06 12:21:26 +0530

    Add files via upload

---
+++ Demo/TestBundle/src/DemoTestBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Demo\TestBundle;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+
+class DemoTestBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+{
+    use BundleAdminClassicTrait;
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    public function getJsPaths(): array
+    {
+        return [
+            '/bundles/demotest/js/pimcore/startup.js'
+        ];
+    }
+
+}
